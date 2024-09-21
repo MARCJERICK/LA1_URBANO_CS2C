f66f587a118088b9223f109483718ef102dabed0
Marc Jerick <umarcjerick@gmail.com>
2024-09-21 21:27:41 +0800

    Complete Variable Section

--- activity3.js
+++ activity3.js
@@ -1,5 +1,14 @@
+// Declare a boolean variable indicating if JavaScript is a programming language
 let isJsProgrammingLanguage = true;
+
+// Declare a boolean variable indicating if JavaScript is hard
 let isJsHard = false;
+
+// Declare a variable to store a favorite number
 let favnumb = 16;
+
+// Declare a variable with an undefined value
 let favNumb = undefined;
-console.log("isJsProgrammingLanguage ", + "isJsHard ", + "favnumb ", + "favNumb");
+
+// Log the values of all the variables, ensuring they are separated by commas
+console.log(isJsProgrammingLanguage, isJsHard, favnumb, favNumb);
