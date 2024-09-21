eef835a863f9f5e5e8f8e4ff848c88ba8be0751b
Marc Jerick <umarcjerick@gmail.com>
2024-09-21 20:44:53 +0800

    initial with activity1.js

---
+++ activity1.js
@@ -0,0 +1,18 @@
+// Declare a variable to store the full name
+let fullname = "Marc Jerick Marbel Urbano";
+
+// Declare a variable to store the answer to the question about future goals
+let WhatDoYouWannaBecomeInYouLife = "Programmer";
+
+// Declare a variable to store the gender
+let Gender = "Male";
+
+// Declare a variable to store the Facebook handle
+let facebookhandle = "Marc Jerick";
+
+// Log the full name,future goal,Gender and lastly facebookhandle
+console.log(fullname);
+console.log(WhatDoYouWannaBecomeInYouLife);
+console.log(Gender);
+console.log(facebookhandle);
+
