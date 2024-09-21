55fb94fe043748f5cfdd43d3a5e12492482ab48a
Marc Jerick <umarcjerick@gmail.com>
2024-09-21 20:59:23 +0800

    initial with activity2.js

---
+++ activity2.js
@@ -0,0 +1,15 @@
+const firstfavnum = 16;
+const secondfavnum = 19;
+let add = firstfavnum + secondfavnum;
+let sub = firstfavnum - secondfavnum;
+let mult = firstfavnum * secondfavnum;
+let div = firstfavnum / secondfavnum;
+let mod = firstfavnum % secondfavnum;
+let power = firstfavnum ** secondfavnum;
+
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(mod);
+console.log(power);
