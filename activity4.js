9227486629e61ef1bb714f819411d7e1c5403bb0
Marc Jerick <umarcjerick@gmail.com>
2024-09-21 21:47:05 +0800

    Complete Variable Section

---
+++ activity4.js
@@ -0,0 +1,28 @@
+// Declare a constant for the first and second favorite number
+let firstfavnum = 16;
+let secondfavnum = 19;
+// Compare if the first number is greater than the second number
+let Gtnan = firstfavnum > secondfavnum;
+// Compare if the first number is less than the second number
+let Lthan = firstfavnum < secondfavnum;
+// Compare if the first number is greater than or equal to the second number
+let Gthaneq = firstfavnum >= secondfavnum;
+// Compare if the first number is less than or equal to the second number
+let Lthaneq = firstfavnum <= secondfavnum;
+// Strict equality check 
+let Striceq = firstfavnum === secondfavnum;
+// Loose equality check 
+let looseeq = firstfavnum == secondfavnum;
+// Strict inequality check 
+let stricineq = firstfavnum !== secondfavnum;
+// Loose inequality check 
+let looseineq = firstfavnum != secondfavnum;
+// Log the result of each comparison
+console.log(Gtnan);
+console.log(Lthan);
+console.log(Gthaneq);
+console.log(Lthaneq);
+console.log(stricineq);
+console.log(looseeq);
+console.log(stricineq);
+console.log(looseineq);
