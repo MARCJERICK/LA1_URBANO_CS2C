87a520e4457b8deb656ebe22256e3c6b12e508ab
Marc Jerick <umarcjerick@gmail.com>
2024-09-21 21:25:33 +0800

    Complete Variable Section

--- activity3.js
+++ activity3.js
@@ -2,3 +2,4 @@ let isJsProgrammingLanguage = true;
 let isJsHard = false;
 let favnumb = 16;
 let favNumb = undefined;
+console.log("isJsProgrammingLanguage ", + "isJsHard ", + "favnumb ", + "favNumb");
